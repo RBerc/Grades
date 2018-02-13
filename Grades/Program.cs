16e3630ce1d2fef335f6e32e296f8300858b255e
Robert Bercovic <rbercovic@gmail.com>
2018-02-12 22:21:34 -0500

    Some Speech and some Unit Tests

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace Grades
 {
@@ -10,10 +11,23 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello! This is the grade book program");
+
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
-          
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            synth.Speak("The Average Grade is" + stats.AverageGrade);
+            Console.WriteLine(stats.AverageGrade);
+            synth.Speak("The Highest Grade is" + stats.HighestGrade);
+            Console.WriteLine(stats.HighestGrade);
+            synth.Speak("The Lowest Grade is" + stats.LowestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.ReadKey();
+
         }
     }
 }
