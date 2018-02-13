b05dc9a80d949d4cef4b6e9b8717c927c3702cc3
Robert Bercovic <rbercovic@gmail.com>
2018-02-12 20:21:53 -0500

    Added Constructor

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,6 +8,13 @@ namespace Grades
 {
     class GradeBook
     {
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
